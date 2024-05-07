0606b51ec821c365eb80c55bc31368906bb7b512
fbwougr121 <fbwogur121@naver.com>
2024-05-07 19:47:44 +0900

    dd

--- src/main/kotlin/com/capstone/jachulsa/JachulsaApplication.kt
+++ src/main/kotlin/com/capstone/jachulsa/JachulsaApplication.kt
@@ -23,15 +23,15 @@ fun main(args: Array<String>) {
 fun init(repository: UserRepository): CommandLineRunner {
 	return CommandLineRunner { args: Array<String?>? ->
 		val user = User(
-				email = "test@test.com",
-				name = "Test User",
-				nickname = "test",
-				sex = "M",
-				birthdate = LocalDate.now(),
-				is_active = true,
-				is_public = true,
-				address = Address(city = "Seoul", detail_address = "123-456"),
-				total_riding = TotalRiding(total_distance_meters = 1000, total_riding_minutes = 60, total_CO2_grams = 500, total_calories = 200)
+			email = "test@test.com",
+			name = "Test User",
+			nickname = "test",
+			sex = "M",
+			birthdate = LocalDate.now(),
+			is_active = true,
+			is_public = true,
+			address = Address(city = "Seoul", detail_address = "123-456"),
+			total_riding = TotalRiding(total_distance_meters = 1000, total_riding_minutes = 60, total_CO2_grams = 500, total_calories = 200)
 		)
 		repository.save(user)
 	}
