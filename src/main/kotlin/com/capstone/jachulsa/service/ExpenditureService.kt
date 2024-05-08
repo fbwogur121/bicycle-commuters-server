ef5b4f8d52e44a9ee788c0ad3e3fd0e1d2e7a545
Hyunsoo Choi <qjtjttlfj2@naver.com>
2024-05-08 18:58:31 +0900

    expenditure 수정

--- src/main/kotlin/com/capstone/jachulsa/service/ExpenditureService.kt
+++ src/main/kotlin/com/capstone/jachulsa/service/ExpenditureService.kt
@@ -4,6 +4,7 @@ import com.capstone.jachulsa.domain.Expenditure
 import com.capstone.jachulsa.dto.ResponseCode
 import com.capstone.jachulsa.exception.CustomException
 import com.capstone.jachulsa.repository.ExpenditureRepository
+import com.capstone.jachulsa.repository.UserRepository
 import org.bson.types.ObjectId
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -11,16 +12,28 @@ import org.springframework.stereotype.Service
 import java.time.LocalDate
 
 @Service
-class ExpenditureService(private val expenditureRepository: ExpenditureRepository) {
+class ExpenditureService(private val expenditureRepository: ExpenditureRepository,private val userRepository: UserRepository) {
 
-    fun createExpenditure(expenditure: Expenditure):ObjectId? {
+    fun createExpenditure(expenditure: Expenditure): ObjectId? {
+        val user = userRepository.findById(expenditure.userId)
+            .orElseThrow { CustomException(ResponseCode.USER_NOT_FOUND) }
         return expenditureRepository.save(expenditure).expenditureId
     }
 
-    fun getExpenditures(userId: String, startDate: LocalDate, endDate: LocalDate,  pageable: Pageable): Page<Expenditure> { //myExpenditureOnly: Boolean,
+    fun getExpenditures(
+        userId: String,
+        startDate: LocalDate,
+        endDate: LocalDate,
+        pageable: Pageable
+    ): Page<Expenditure> { //myExpenditureOnly: Boolean,
+
+        val user = userRepository.findById(userId)
+            .orElseThrow { CustomException(ResponseCode.USER_NOT_FOUND) }
+
         val expenditures = expenditureRepository.findByUserIdAndDateBetween(userId, startDate, endDate, pageable)
         if (expenditures.isEmpty) {
-            throw CustomException(ResponseCode.RESOURCE_NOT_FOUND)
+            throw CustomException(ResponseCode.EXPENDITURE_NOT_FOUND)
         }
-        return expenditures}
+        return expenditures
+    }
 }
