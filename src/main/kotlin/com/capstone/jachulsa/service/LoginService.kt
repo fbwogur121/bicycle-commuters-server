4c897adfef8a2c6c4024292580ecc15984605e74
fbwougr121 <fbwogur121@naver.com>
2024-05-08 23:20:51 +0900

    return

--- src/main/kotlin/com/capstone/jachulsa/service/LoginService.kt
+++ src/main/kotlin/com/capstone/jachulsa/service/LoginService.kt
@@ -4,14 +4,9 @@ import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
 import org.springframework.web.util.UriComponentsBuilder
 import java.io.BufferedReader
-import java.io.UnsupportedEncodingException
-import java.net.MalformedURLException
-import java.net.URISyntaxException
-import java.net.URLEncoder
-
 import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import java.io.UnsupportedEncodingException
+import java.net.*
 import java.util.stream.Collectors
 
 
@@ -22,7 +17,7 @@ class LoginService {
     fun getNaverAuthorizeUrl(type: String): String {
         val baseUrl = "https://nid.naver.com/oauth2.0/authorize"
         val clientId = "KsYEPMXctpGK6qPFxnbF"
-        val redirectUrl = "https://api.jachulsa.shop/oauth/login"
+        val redirectUrl = "http://localhost:8080/oauth/login"
         val uriComponents = UriComponentsBuilder
             .fromUriString(baseUrl)
             .queryParam("response_type", "code")
